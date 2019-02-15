6f43a268d8c73831bf08af722044c2eac29f7761
jiazhijie <1074968157@qq.com>
2019-02-15 21:56:59 +0800

    完成登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -18,13 +18,13 @@ export default new Router({
   ]
 })
 //路由导航守卫 只有登录用户才能访问路由后台
-router.beforeEach((to, from, next) => {
-  console.log(this)
-  if (to.path === '/login') return next()
-  //获取token
-  const tokenStr = window.sessionStorage.getItem('token')
+// router.beforeEach((to, from, next) => {
+//   console.log(this)
+//   if (to.path === '/login') return next()
+//   //获取token
+//   const tokenStr = window.sessionStorage.getItem('token')
 
-  if(!tokenStr) return this.a.push('/login')
-  next()
-})
-export default router
+//   if(!tokenStr) return this.a.push('/login')
+//   next()
+// })
+// export default router
